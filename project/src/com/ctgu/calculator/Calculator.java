3191f1401cfafcbfa942aa6a8e1bc6f86d63d11f
gydjsz <250594869@qq.com>
2019-10-10 17:04:06 +0800

    编写求值和判断优先级方法

--- project/src/com/ctgu/calculator/Calculator.java
+++ project/src/com/ctgu/calculator/Calculator.java
@@ -4,6 +4,7 @@ package com.ctgu.calculator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 
 public class Calculator {
     private List<Integer> num;
@@ -33,6 +34,51 @@ public class Calculator {
                     symbol.add("/");
             }
             num.add(r.nextInt(101));
+        }
+    }
+
+    /**
+     *判断优先级
+     */
+    public int isFirst(String s){
+        switch(s){
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * 求值
+     * @return a ? b
+     */
+    public int calSymbol(int a, int b, String s){
+        switch(s){
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+
+    public void getResult(){
+        Stack<Integer> sn = new Stack<>();
+        Stack<String> ss = new Stack<>();
+
+        int a, b, c;
+        for(int i = 0; i < num.size() + symbol.size(); ++i){
+
         }
     }
 
